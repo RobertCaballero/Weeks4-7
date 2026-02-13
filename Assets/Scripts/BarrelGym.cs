c4cd9f7e2870addc9d89e1a4f91c45b11ab5ab6a
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-02-13 17:42:45 -0500

    New improved gym

--- Assets/Scripts/BarrelGym.cs
+++ Assets/Scripts/BarrelGym.cs
@@ -5,25 +5,43 @@ using System.Collections.Generic;
 public class BarrelGym : MonoBehaviour
 {
 
-    public List<GameObject> knife = new List<GameObject>();
+    public List<GameObject> currentKnives = new List<GameObject>();
 
     public GameObject spawnKnifes;
 
+    Vector2 v = new Vector2(2.12f, -0.86f);
+    Vector2 v2 = new Vector2(2.32f, 1.36f);
+    Vector2 v3 = new Vector2(-2.84f, 1.16f);
+    Vector2 v5 = new Vector2(-2.87f, -0.76f);
+
+ 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       for (int i = 0; i < knife.Count; i++)
-        {
-            GameObject currentKnifes = knife[i];
-            Debug.Log(currentKnifes);
+       //for (int i = 0; i < knife.Count; i++)
+       // {
+       //     GameObject currentKnifes = knife[i];
+       //     Debug.Log(currentKnifes);
 
-        }
+       // }
 
 
         GameObject spawnObject = Instantiate(spawnKnifes, transform.position, Quaternion.identity);
-        knife.Add(spawnObject);
+        GameObject spawnObject2 = Instantiate(spawnKnifes, v, Quaternion.identity);
+        GameObject spawnObject3 = Instantiate(spawnKnifes, v2, Quaternion.identity);
+        GameObject spawnObject4 = Instantiate(spawnKnifes, v3, Quaternion.identity);
+        GameObject spawnObject5 = Instantiate(spawnKnifes, v5, Quaternion.identity);
+
+        currentKnives.Add(spawnObject);
+        currentKnives.Add(spawnObject2);
+        currentKnives.Add(spawnObject3);
+        currentKnives.Add(spawnObject4);
+        currentKnives.Add(spawnObject5);
+
+
+
     }
 
     // Update is called once per frame
