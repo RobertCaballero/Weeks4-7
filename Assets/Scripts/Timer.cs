178037c564a7e4a08de1d156038ff862925cb2c8
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-02-17 22:56:53 -0500

    Saving

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,20 +7,17 @@ public class Timer : MonoBehaviour
 
     public float timerValue = 10;
     public float timerMinValue = 0;
-    //public float IncreaseSpeedValue = 5f;
-    //public float IncreaseSpeedMinValue = 0;
     private float speed = 1f;
     private float currentSpeed = 1f;
     private float increaseSpeed = 1f;
 
     public Slider ClockTimer;
-    //public Slider IncreaseSpeed;
+ 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ClockTimer.minValue = timerMinValue;
-        //IncreaseSpeed.maxValue = IncreaseSpeedValue;
     }
 
     // Update is called once per frame
@@ -36,14 +33,7 @@ public class Timer : MonoBehaviour
 
         ClockTimer.value = timerValue;
         currentSpeed = speed + increaseSpeed;
-        
-        
     }
-
-    //public void ChangeSpeed(float value)
-    //{
-
-    //}
 
     public void Speed (float value)
     {
