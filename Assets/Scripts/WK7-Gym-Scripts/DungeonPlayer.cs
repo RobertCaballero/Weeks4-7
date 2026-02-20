c3c4ea4afc530d07ea7fd6e8adb96a12c0612b8c
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-02-20 18:15:32 -0500

    GYM

--- Assets/Scripts/WK7-Gym-Scripts/DungeonPlayer.cs
+++ Assets/Scripts/WK7-Gym-Scripts/DungeonPlayer.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DungeonPlayer : MonoBehaviour
 {
+    public int Health;
+    private float speed = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,45 @@ public class DungeonPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool leftArrowHeld = Keyboard.current.leftArrowKey.isPressed;
+        bool rightArrowHeld = Keyboard.current.rightArrowKey.isPressed;
+        bool upArrowHeld = Keyboard.current.upArrowKey.isPressed;
+        bool downArrowHeld = Keyboard.current.downArrowKey.isPressed;
+
+        if (leftArrowHeld)
+        {
+
+            transform.position -= transform.right * speed * Time.deltaTime;
+
+        }
+
+
+        if (rightArrowHeld)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (upArrowHeld)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (downArrowHeld)
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+
+        if (Health < 0)
+        {
+            Destroy(gameObject);
+        }
+
     }
+
+    public void TakingDamage(int damage)
+    {
+        Health -= damage;
+    }
+
+
 }
--- Assets/Scripts/WK7-Gym-Scripts/Snake.cs
+++ Assets/Scripts/WK7-Gym-Scripts/Snake.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+
+    public int damage;
+    public DungeonPlayer player;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+
+    }
+
+    public void ApplyDamage()
+    {
+        player.TakingDamage(damage);
     }
 }
